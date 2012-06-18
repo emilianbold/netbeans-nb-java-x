27a4a9f82020df8d03dfd1d6a1e1d0a8069f41b4
Dusan Balek <dbalek@netbeans.org>
2012-06-18 09:19:36 +0200

    Issue #182009: Runtime error occured due to Uncompilable source code - fixed.

--- langtools/src/share/classes/com/sun/tools/javac/comp/Repair.java
+++ langtools/src/share/classes/com/sun/tools/javac/comp/Repair.java
@@ -569,11 +569,14 @@ public class Repair extends TreeTranslator {
                             tree.defs = tree.defs.prepend(generateErrStaticInit(classLevelErrTree, classLevelErrMessage));
                     }
                     for (MethodSymbol symbol : nonAbstractMethods) {
+                        if ((symbol.flags() & Flags.BRIDGE) != 0) {
+                            continue;
+                        }
                         if ((symbol.owner.flags() & Flags.ENUM) != 0) {
                             if ((symbol.name == symbol.name.table.names.values
                                     && symbol.type.asMethodType().argtypes.isEmpty())
                                     || (symbol.name == symbol.name.table.names.valueOf
-                                    && symbol.type.asMethodType().argtypes.head == enter.syms.stringType
+                                    && types.isSameType(symbol.type.asMethodType().argtypes.head, enter.syms.stringType)
                                     && symbol.type.asMethodType().argtypes.tail.isEmpty())) {
                                 continue;
                             }
