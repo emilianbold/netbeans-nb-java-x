ac8f13ae3c94e5c11ba49b99eb1d552adee1ae86
Dusan Balek <dbalek@netbeans.org>
2013-03-08 19:32:25 +0100

    Enhanced error recovery for intersection types in cast expressions.

--- langtools/src/share/classes/com/sun/tools/javac/comp/Attr.java
+++ langtools/src/share/classes/com/sun/tools/javac/comp/Attr.java
@@ -2907,56 +2907,115 @@ public class Attr extends JCTree.Visitor {
     }
 
     public void visitBinary(JCBinary tree) {
-        // Attribute arguments.
-        Type left = chk.checkNonVoid(tree.lhs.pos(), attribExpr(tree.lhs, env));
-        Type right = chk.checkNonVoid(tree.lhs.pos(), attribExpr(tree.rhs, env));
+        boolean baCatched = false;
+        try {
+            // Attribute arguments.
+            Type left = chk.checkNonVoid(tree.lhs.pos(), attribExpr(tree.lhs, env));
+            Type right = chk.checkNonVoid(tree.lhs.pos(), attribExpr(tree.rhs, env));
 
-        // Find operator.
-        Symbol operator = tree.operator =
-            rs.resolveBinaryOperator(tree.pos(), tree.getTag(), env, left, right);
+            // Find operator.
+            Symbol operator = tree.operator =
+                rs.resolveBinaryOperator(tree.pos(), tree.getTag(), env, left, right);
 
-        Type owntype = types.createErrorType(tree.type);
-        if (operator.kind == MTH &&
-                !left.isErroneous() &&
-                !right.isErroneous()) {
-            owntype = operator.type.getReturnType();
-            int opc = chk.checkOperator(tree.lhs.pos(),
-                                        (OperatorSymbol)operator,
-                                        tree.getTag(),
-                                        left,
-                                        right);
+            Type owntype = types.createErrorType(tree.type);
+            if (operator.kind == MTH &&
+                    !left.isErroneous() &&
+                    !right.isErroneous()) {
+                owntype = operator.type.getReturnType();
+                int opc = chk.checkOperator(tree.lhs.pos(),
+                                            (OperatorSymbol)operator,
+                                            tree.getTag(),
+                                            left,
+                                            right);
 
-            // If both arguments are constants, fold them.
-            if (left.constValue() != null && right.constValue() != null) {
-                Type ctype = cfolder.fold2(opc, left, right);
-                if (ctype != null) {
-                    owntype = cfolder.coerce(ctype, owntype);
+                // If both arguments are constants, fold them.
+                if (left.constValue() != null && right.constValue() != null) {
+                    Type ctype = cfolder.fold2(opc, left, right);
+                    if (ctype != null) {
+                        owntype = cfolder.coerce(ctype, owntype);
 
-                    // Remove constant types from arguments to
-                    // conserve space. The parser will fold concatenations
-                    // of string literals; the code here also
-                    // gets rid of intermediate results when some of the
-                    // operands are constant identifiers.
-                    if (tree.lhs.type.tsym == syms.stringType.tsym) {
-                        tree.lhs.type = syms.stringType;
-                    }
-                    if (tree.rhs.type.tsym == syms.stringType.tsym) {
-                        tree.rhs.type = syms.stringType;
+                        // Remove constant types from arguments to
+                        // conserve space. The parser will fold concatenations
+                        // of string literals; the code here also
+                        // gets rid of intermediate results when some of the
+                        // operands are constant identifiers.
+                        if (tree.lhs.type.tsym == syms.stringType.tsym) {
+                            tree.lhs.type = syms.stringType;
+                        }
+                        if (tree.rhs.type.tsym == syms.stringType.tsym) {
+                            tree.rhs.type = syms.stringType;
+                        }
                     }
                 }
-            }
 
-            // Check that argument types of a reference ==, != are
-            // castable to each other, (JLS???).
-            if ((opc == ByteCodes.if_acmpeq || opc == ByteCodes.if_acmpne)) {
-                if (!types.isCastable(left, right, new Warner(tree.pos()))) {
-                    log.error(tree.pos(), "incomparable.types", left, right);
+                // Check that argument types of a reference ==, != are
+                // castable to each other, (JLS???).
+                if ((opc == ByteCodes.if_acmpeq || opc == ByteCodes.if_acmpne)) {
+                    if (!types.isCastable(left, right, new Warner(tree.pos()))) {
+                        log.error(tree.pos(), "incomparable.types", left, right);
+                    }
+                }
+
+                chk.checkDivZero(tree.rhs.pos(), operator, right);
+            }
+            result = check(tree, owntype, VAL, resultInfo);
+        } catch (BreakAttr ba) {
+            baCatched = true;
+            throw ba;
+        } finally {
+            if ((baCatched || result.isErroneous()) && tree.hasTag(BITAND)) {
+                //error recovery
+                List<JCExpression> bounds = collectIntersectionBounds(tree);
+                if (bounds != null) {
+                    attribTypes(bounds, env);
+                    Type owntype = checkIntersection(tree, bounds);
+                    if (!owntype.isErroneous())
+                        tree.type = owntype;
                 }
             }
-
-            chk.checkDivZero(tree.rhs.pos(), operator, right);
         }
-        result = check(tree, owntype, VAL, resultInfo);
+    }
+    
+    private List<JCExpression> collectIntersectionBounds(JCTree tree) {
+        if (tree.hasTag(BITAND)) {
+            List<JCExpression> left = collectIntersectionBounds(((JCBinary)tree).lhs);
+            if (left != null) {
+                List<JCExpression> right = collectIntersectionBounds(((JCBinary)tree).rhs);
+                if (right != null) {
+                    return left.appendList(right);
+                }
+            }
+        } else if (tree instanceof JCExpression) {
+            return List.of(new TreeTranslator() {
+                @Override
+                public <T extends JCTree> T translate(T tree) {
+                    if (tree != null && tree.type.isErroneous()) {
+                        tree.type = null;
+                        return super.translate(tree);
+                    }
+                    return tree;
+                }
+
+                @Override
+                public void visitIdent(JCIdent tree) {
+                    tree.sym = null;
+                    super.visitIdent(tree);
+                }
+
+                @Override
+                public void visitSelect(JCFieldAccess tree) {
+                    tree.sym = null;
+                    super.visitSelect(tree);
+                }
+
+                @Override
+                public void visitReference(JCMemberReference tree) {
+                    tree.sym = null;
+                    super.visitReference(tree);
+                }                
+            }.translate((JCExpression)tree));
+        }
+        return null;
     }
 
     public void visitTypeCast(final JCTypeCast tree) {
