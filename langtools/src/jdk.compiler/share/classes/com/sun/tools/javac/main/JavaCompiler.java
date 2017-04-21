ac8fd4d9426e77d6653140d6744b1301fa108386
Dusan Balek <dbalek@netbeans.org>
2017-04-21 13:18:55 +0200

    Issue #269836 - NullPointerException at com.sun.tools.javac.processing.JavacProcessingEnvironment$Round.unrecoverableError - fixed.

--- langtools/src/jdk.compiler/share/classes/com/sun/tools/javac/main/JavaCompiler.java
+++ langtools/src/jdk.compiler/share/classes/com/sun/tools/javac/main/JavaCompiler.java
@@ -871,24 +871,30 @@ public class JavaCompiler {
                                                  JavaFileObject.Kind.SOURCE);
             if (isModuleInfo) {
                 if (enter.getEnv(tree.modle) == null) {
-                    if (h != null)
+                    if (h != null) {
                         log.popDiagnosticHandler(h);
+                        deferredDiagnosticHandler = null;
+                    }
                     JCDiagnostic diag =
                         diagFactory.fragment("file.does.not.contain.module");
                     throw new ClassFinder.BadClassFile(c, filename, diag, diagFactory);
                 }
             } else if (isPkgInfo) {
                 if (enter.getEnv(tree.packge) == null) {
-                    if (h != null)
+                    if (h != null) {
                         log.popDiagnosticHandler(h);
+                        deferredDiagnosticHandler = null;
+                    }
                     JCDiagnostic diag =
                         diagFactory.fragment("file.does.not.contain.package",
                                                  c.location());
                     throw new ClassFinder.BadClassFile(c, filename, diag, diagFactory);
                 }
             } else {
-                if (h != null)
+                if (h != null) {
                     log.popDiagnosticHandler(h);
+                    deferredDiagnosticHandler = null;
+                }
                 JCDiagnostic diag =
                         diagFactory.fragment("file.doesnt.contain.class",
                                             c.getQualifiedName());
