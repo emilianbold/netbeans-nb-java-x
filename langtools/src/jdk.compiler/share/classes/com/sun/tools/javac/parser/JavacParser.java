a19dcd474eae89b3c97dc1f10ef3fc1cb88bd4c6
Dusan Balek <dbalek@netbeans.org>
2017-04-25 14:26:41 +0200

    Enhanced error recovery.

--- langtools/src/jdk.compiler/share/classes/com/sun/tools/javac/parser/JavacParser.java
+++ langtools/src/jdk.compiler/share/classes/com/sun/tools/javac/parser/JavacParser.java
@@ -3559,6 +3559,14 @@ public class JavacParser implements Parser {
             nextToken();
             extending = parseType();
         }
+        
+        if (token.kind != IMPLEMENTS && token.kind != LBRACE) {
+            syntaxError(token.pos, "expected2", LBRACE, IMPLEMENTS);
+            do {
+                nextToken();
+            } while (token.kind != IMPLEMENTS && token.kind != LBRACE);
+        }
+
         List<JCExpression> implementing = List.nil();
         if (token.kind == IMPLEMENTS) {
             nextToken();
